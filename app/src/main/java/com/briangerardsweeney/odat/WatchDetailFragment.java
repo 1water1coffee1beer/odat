c1e97fd646e3f827d3a4d680a6e2180c2230b146
Brian Sweeney <briangsweeney@gmail.com>
2014-12-29 17:16:38 -0500

    first commit

---
+++ app/src/main/java/com/briangerardsweeney/odat/WatchDetailFragment.java
@@ -0,0 +1,70 @@
+package com.briangerardsweeney.odat;
+
+import android.os.Bundle;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+
+import com.briangerardsweeney.odat.dummy.DummyContent;
+
+/**
+ * A fragment representing a single Watch detail screen.
+ * This fragment is either contained in a {@link WatchListActivity}
+ * in two-pane mode (on tablets) or a {@link WatchDetailActivity}
+ * on handsets.
+ */
+public class WatchDetailFragment extends Fragment {
+    /**
+     * The fragment argument representing the item ID that this fragment
+     * represents.
+     */
+    public static final String ARG_ITEM_ID = "item_id";
+
+    /**
+     * The dummy content this fragment is presenting.
+     */
+    private DummyContent.DummyItem mItem;
+
+    /**
+     * Mandatory empty constructor for the fragment manager to instantiate the
+     * fragment (e.g. upon screen orientation changes).
+     */
+    public WatchDetailFragment() {
+        setHasOptionsMenu(true);
+    }
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        if (getArguments().containsKey(ARG_ITEM_ID)) {
+            // Load the dummy content specified by the fragment
+            // arguments. In a real-world scenario, use a Loader
+            // to load content from a content provider.
+            mItem = DummyContent.ITEM_MAP.get(getArguments().getString(ARG_ITEM_ID));
+        }
+    }
+
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,
+                             Bundle savedInstanceState) {
+        View rootView = inflater.inflate(R.layout.fragment_watch_detail, container, false);
+
+        // Show the dummy content as text in a TextView.
+        if (mItem != null) {
+            ((TextView) rootView.findViewById(R.id.watch_detail)).setText(mItem.content);
+        }
+
+        return rootView;
+    }
+
+    @Override
+    public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
+        inflater.inflate(R.menu.watch_detail_actions, menu);
+    }
+}
